ba227ebcbd98126e8039bf0011570ccbb83b1353
jetgive43 <47237077+jetgive43@users.noreply.github.com>
2020-06-09 18:44:38 +0530

    Update test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,3 @@
-# frozen_string_literal: true
-
-require File.expand_path('../config/environment', __dir__)
-require 'rails/test_help'
-
-require 'simplecov'
-SimpleCov.start
-
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+class ActiveSupport::TestCase
+  parallelize(workers: 2)
 end
-
-ActiveSupport::TestCase.test_order = :random
