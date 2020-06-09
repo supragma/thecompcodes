23af4c64937caf23fc6f4e3ba92e273c744acce3
jetgive43 <47237077+jetgive43@users.noreply.github.com>
2020-06-09 17:12:06 +0530

    Fix tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,10 +14,7 @@ end
 module Test
   class ActiveSupport
     class TestCase
-      # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-      fixtures :all
 
-      # Add more helper methods to be used by all tests here...
     end
   end
 end
