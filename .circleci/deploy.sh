40e8ac636da89d3c8e501362128f8de0ade4674c
Ashish Belwase <belwasetech@Ashishs-MacBook-Pro.local>
2020-06-16 09:44:50 +0545

    used domain name on deployment, new lines

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -5,12 +5,12 @@
 if [ $1 == "master" ]
 then
 	echo "Deploying to $1"
-	ssh -o StrictHostKeyChecking=no -i id_rsa belwasetech@34.71.79.243 "bash -s" <./.circleci/commands.sh $1
+	ssh -o StrictHostKeyChecking=no -i id_rsa belwasetech@comp.codes "bash -s" <./.circleci/commands.sh $1
 elif [ $1 == "stage" ]
 then
 	echo "Deploying to $1"
-	ssh -o StrictHostKeyChecking=no -i id_rsa belwasetech@34.72.200.164 "bash -s" <./.circleci/commands.sh $1
+	ssh -o StrictHostKeyChecking=no -i id_rsa belwasetech@stage.comp.codes "bash -s" <./.circleci/commands.sh $1
 else
 	echo "Deploying to $1"
-	ssh -o StrictHostKeyChecking=no -i id_rsa belwasetech@104.198.152.241 "bash -s" <./.circleci/commands.sh $1
+	ssh -o StrictHostKeyChecking=no -i id_rsa belwasetech@dev.comp.codes "bash -s" <./.circleci/commands.sh $1
 fi
