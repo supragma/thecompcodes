5bab6ca225ae6ce723fe9b2bed317325be3e59bb
Navraj Chohan <nlake44@gmail.com>
2020-08-02 13:04:07 -0700

    Remove welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
-
   # Set paths for the API.
   namespace :api, default: {format: :json} do
     namespace :v1 do
