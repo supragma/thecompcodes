410b2b4c62a81f2c68157b0a31320ec13f0528a4
Navraj Chohan <nlake44@gmail.com>
2020-08-02 14:05:11 -0700

    Check JSON response

--- test/controllers/liveness_controller_test.rb
+++ test/controllers/liveness_controller_test.rb
@@ -5,11 +5,13 @@ class LivenessControllerTest < ActionDispatch::IntegrationTest
   test "test failure liveness" do
     get api_v1_failure_url
     assert_response 400
+    assert_not JSON.parse(@response.body)["success"]
   end
 
   test "test success liveness" do
     get api_v1_liveness_url
     assert_response 200
+    assert JSON.parse(@response.body)["success"]
   end
   
 end
