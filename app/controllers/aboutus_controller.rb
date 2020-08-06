a01730810ba12ecd6a506bb62b328e9567383fe9
Navraj Chohan <nlake44@gmail.com>
2020-08-06 15:44:31 -0700

    Add footer

---
+++ app/controllers/aboutus_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Controller for the root view.
+class AboutusController < ApplicationController
+
+  # About Us view method.
+  def index
+    render 'index'
+  end
+end
