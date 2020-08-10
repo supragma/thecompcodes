f2eed25902cef7a80ef05724a427ffb24c311228
Navraj Chohan <nlake44@gmail.com>
2020-08-10 15:51:13 -0700

    Update production environment configs for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,5 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'thecompcode.com', port: 443}
+  config.action_mailer.default_url_options = { host: 'thecompcodes.com', port: 443}
+  config.action_mailer.delivery_method = :smtp
 end
