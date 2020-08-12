9ffea4fc571da947f3085fcd6c1c209d4127b470
Navraj Chohan <nlake44@gmail.com>
2020-08-12 08:15:52 -0700

    Add tests for get it now page

---
+++ test/controllers/getitnow_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class GetitnowControllerTest < ActionDispatch::IntegrationTest
+
+  test "test getting contact us page" do
+    get getitnow_url
+    assert_response 200
+  end
+
+  test "test posting contact us page" do
+    post getitnow_url, params: { email: "some@email.com",
+                                 name: "myname",
+                                 phone: "123456789" }
+    assert @response.redirect_url.include? "contactus"
+  end
+end
