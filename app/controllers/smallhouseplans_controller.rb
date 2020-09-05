fbc7d41a3482ffd267fb425844b46fa464b6d835
Navraj Chohan <nlake44@gmail.com>
2020-09-05 11:38:53 -0700

    Add path for small house plans

---
+++ app/controllers/smallhouseplans_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Controller for the small house plans landing page.
+class SmallhouseplansController < ApplicationController
+
+  # Root view method.
+  def index
+    render 'index'
+  end
+end
