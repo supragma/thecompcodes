d40f2c120181450c9a08170a4ebb15b176c432dd
Navraj Chohan <nlake44@gmail.com>
2020-09-17 10:33:34 -0700

    Improvements on PDF generation

--- app/mailers/send_quote_mailer.rb
+++ app/mailers/send_quote_mailer.rb
@@ -51,31 +51,44 @@ class SendQuoteMailer < ApplicationMailer
         pdf.text "www.thecompcodes.com", :align => :left
       end
       pdf.text "Scope of Work", :size => 14
+      pdf.text "\u2022 #{quote.details}"
       pdf.text "\u2022 #{first_bullet_point(quote)}"
       pdf.text "\u2022 #{second_bullet_point(quote)}"
       if third_bullet_point(quote) != ""
         pdf.text "\u2022 #{third_bullet_point(quote)}"
       end
-      if quote.earth_work
-        pdf.text "\u2022 Project requires Topography Survey."
+      if quote.site_improvements
+        pdf.text "\u2022 Project requires Civil Engineering."
+      elsif quote.earth_work
+        pdf.text "\u2022 Project requires Topography Survey and Civil Engineering."
       else
         pdf.text "\u2022 Should the Project Require Topography Survey, or Civil Engineering Set we can offer it at an additional cost."
       end
+
+      if quote.interior_alt || quote.exterior_alt
+        pdf.text "\u2022 Project requires Structural Engineering."
+      end
+
       pdf.stroke_horizontal_rule
       pdf.move_down 10
       pdf.text "Details of Invoice", :style => :bold_italic
       pdf.stroke_horizontal_rule
-      total_price = 0
+
       item = 1
-      temp_arr = [{:name => "Item #{item.to_s}\nArch Sets", :price => get_price(get_size(quote.size)).to_s}]
-      total_price += get_price_integer(get_size(quote.size))
+      total_price = 0
+      temp_arr = [{:name => "Item #{item.to_s}\nArch Sets\n#{arch_set_description(quote)}", :price => get_line_item_one_price(get_size(quote.size)).to_s}]
+      total_price += get_size(quote.size)
       item += 1
       if quote.earth_work
-        temp_arr.append({:name => "Item #{item.to_s}\nTopo Sets", :price => get_price(get_size(quote.size)).to_s})
-        total_price += get_price_integer(get_size(quote.size))
+        temp_arr.append({:name => "Item #{item.to_s}\nTopo Sets", :price => get_price_topography(get_size(quote.size)).to_s})
+        total_price += get_size(quote.size)
         item += 1
       end
-       
+      if quote.site_improvements || quote.earth_work
+        temp_arr.append({:name => "Item #{item.to_s}\nCivil Engineering Sets", :price => get_price_civil(get_size(quote.size)).to_s})
+        total_price += get_size(quote.size)
+        item += 1
+      end   
       pdf.move_down 10
       items = [["No","Description", "Qt.", "Cost"]]
            items += temp_arr.each_with_index.map do |item, i|
@@ -175,13 +188,11 @@ class SendQuoteMailer < ApplicationMailer
       text += "Sewer, "
       number_of_items_added += 1
     end
+
     if number_of_items_added == 0
       return ""
-    elsif number_of_items_added == 1
-      # Convert commas to periods.
-      text.sub(".", ",")
     else
-      text.delete_suffix(",")
+      text = text.chomp(", ")
       text << "."
     end
     return text
@@ -205,11 +216,19 @@ class SendQuoteMailer < ApplicationMailer
     end
   end
 
-  def get_price(footage)
+  def get_line_item_one_price(footage)
     "$" + (footage * 4).to_s
   end
 
-  def get_price_integer(footage)
-    footage * 4
+  def get_price_topography(footage)
+    "$" + (footage * 5).to_s
+  end
+
+  def get_price_civil(footage)
+    "$" + (footage * 5).to_s
+  end
+
+  def arch_set_description(quote)
+    "Arch description"
   end
 end
